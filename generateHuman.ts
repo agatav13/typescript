d1c5703b56c0bbb704accb908a5c241a9620a9a4
agata <agataomasta05@gmail.com>
2024-03-15 23:15:54 +0100

    Function returning 'human' object

---
+++ generateHuman.ts
@@ -0,0 +1,25 @@
+// Chcę aby w tym pliku znalazła się funkcja, która zwróci mi obiekt "człowieka",
+// któremu macie wymyślić jakieś properties do których będziecie w jakiś sposób generować wartości.
+
+
+function range(size: number, start = 0) {
+    return [...Array(size).keys()].map(i => i + start);
+}
+
+function random(possibleValue: Array<any>) {
+    return possibleValue[Math.floor(Math.random() * possibleValue.length)]
+}
+
+const possibleSex = ["female", "male"];
+const possibleAge = range(100, 1);
+const possibleHeight = range(51, 150);
+
+function randomPerson() {
+    return {
+        sex: random(possibleSex),
+        age: random(possibleAge),
+        height: random(possibleHeight)
+    };
+}
+
+console.log(randomPerson())
